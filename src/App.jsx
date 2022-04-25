b4a93f898d8c056e0cf92a23589bce809d75623d
Quentin Guerandel <94524672+QuentinG64@users.noreply.github.com>
2022-04-25 12:11:06 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+function App() {
+  return (
+    <div className="flex items-center justify-center w-screen h-screen text-xl text-white bg-slate-500 text-shadow-md">
+      <p>
+        Ce template est bien configuré pour vos applications front-end avec Vite
+        et Tailwind
+      </p>
+    </div>
+  );
+}
+
+export default App;
