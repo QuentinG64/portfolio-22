328dede2820f9fd4a524dc08f78d38bef2aebb74
Quentin GUERANDEL <quentin.guerandel@gmail.com>
2022-06-07 16:41:04 +0200

    [FEATURE] All screens become responsive

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -11,29 +11,29 @@ const HomePage = () => {
   return (
     <div name="Home" className="w-screen h-screen bg-bg-1">
       <Footer />
-      <div className="flex lg:pt-[15%] md:pt-[25%] justify-center">
+      <div className="md:flex sm:flex sm:flex-row lg:pt-[15%] md:pt-[45%] sm:pt-[67%] justify-center">
         <div className="text-main-2 font-light text-center flex flex-row">
           <Bounce left cascade>
-            <div className="lg:text-4xl sm:text-3xl">Hello, I'm </div>
+            <div className="lg:text-4xl md:text-3xl">Hello, I'm</div>
           </Bounce>
           <Bounce right cascade>
-            <div className="text-main-1 font-reg lg:text-4xl sm:text-3xl">
+            <div className="text-main-1 font-reg md:text-7xl sm:text-4xl pl-2">
               Quentin.
             </div>
           </Bounce>
         </div>
       </div>
 
-      <div className="text-main-2 font-reg pt-8 flex justify-center flex-col text-center">
+      <div className="text-main-2 font-reg md:pt-8 sm:pt-2 flex justify-center flex-col text-center">
         <Fade bottom>
-          <div className="lg:text-6xl md:text-4xl">
+          <div className="lg:text-6xl md:text-4xl sm:text-xl">
             I'm a{" "}
             <span className="text-main-1 font-reg">
               full-stack web developer,
             </span>
           </div>
         </Fade>
-        <div className="text-main-2 font-xlight lg:text-3xl md:text-2xl mt-4">
+        <div className="text-main-2 font-xlight lg:text-3xl md:text-2xl md:mt-4 sm:mt-1">
           <Typewriter
             options={{
               strings: [
